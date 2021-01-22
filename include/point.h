acc1ed6a1f8c5e8863b244afe99add38c094ccd2
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-01-22 14:13:08 +0100

    reworking implementation of Point Class

--- include/point.h
+++ include/point.h
@@ -1,81 +1,36 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <Eigen/Dense>
 #include <cmath>
 #include <iostream>
-
-/**
- * A point may not belong to any cluster.
- * For such cases we deem the point to be
- * Either noise, or unclassified. */
-const int NOISE = -2;
-const int UNCLASSIFIED = -1;
+#include <utility>
+#include <vector>
 
 class Point {
 
-private:
-    bool operator==(const Point* rhs) const
-    {
-        if (this->m_x == rhs->m_x && this->m_y == rhs->m_y
-            && this->m_z == rhs->m_z) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    std::ostream& operator<<(std::ostream& out) const
-    {
-        out << "point at: " << this->m_x << ", " << this->m_y << ", "
-            << this->m_z << "\n inside cluster: " << this->m_cluster;
-        return out;
-    }
-
 public:
+    bool operator==(const Point& rhs) const;
+    friend std::ostream& operator<<(std::ostream& t_stream, const Point& point);
+    friend std::istream& operator>>(std::istream& t_stream, Point& point);
+
+    int m_id;
+    int m_cluster;
+
     float m_x;
     float m_y;
     float m_z;
-    int m_cluster;
-    float m_distance;
 
-    virtual float distance(Point* ptr_other) = 0;
+    Point();
+    Point(float t_x, float t_y, float t_z);
 
-    bool unclassified() const
-    {
-        if (this->m_cluster == UNCLASSIFIED || this->m_cluster == NOISE) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    std::pair<int, float> m_distance;
 
-protected:
-    Point()
-        : m_x(0.0)
-        , m_y(0.0)
-        , m_z(0.0)
-        , m_cluster(UNCLASSIFIED)
-        , m_distance(__DBL_MAX__)
-    {
-    }
+    bool unclassified() const;
+    float distance(Point point) const;
 
-    Point(float t_x, float t_y)
-        : m_x(t_x)
-        , m_y(t_y)
-        , m_cluster(UNCLASSIFIED)
-        , m_distance(__DBL_MAX__)
-    {
-        m_z = 0;
-    }
-
-    Point(float t_x, float t_y, float t_z)
-        : m_x(t_x)
-        , m_y(t_y)
-        , m_z(t_z)
-        , m_cluster(UNCLASSIFIED)
-        , m_distance(__DBL_MAX__)
-    {
-    }
+    static Point centroid(std::vector<Point>& t_points);
+    static bool compare(const Point& t_point, const Point& t_other);
 };
 
 #endif /* POINT_H */
