3bef7e84eddfa668b02eab64de2d8ff908d40c6c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-01-25 13:47:25 +0100

    updating graphing script

--- scripts/graph.py
+++ scripts/graph.py
@@ -8,17 +8,20 @@ from matplotlib.colors import ListedColormap
 
 pwd = os.getcwd()
 
+# KNN:
+data_file = pwd + "/build/output_data.txt"
+plt.figure()
+df = pd.read_csv(data_file)
+sns.lineplot(x=df.id, y=df.nn1, hue = 100)
+plt.xlabel("id")
+plt.ylabel("Euclidean dist")
+plt.title("knn")
+# plt.show()
 
+# DBSCAN:
 data_3d_output = pwd + "/build/results.csv"
-
-# AFTER CLUSTERING:
-# import data
 df = pd.read_csv(data_3d_output)
-
-# figure container
 fig = plt.figure()
-
-# add axes to the figure in subplot arrangement
 ax = fig.add_subplot(111, projection='3d')
 
 x = df["x"]
@@ -26,24 +29,15 @@ y = df["y"]
 z = df["z"]
 c = df["label"]
 
-# figure labels
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 ax.set_zlabel("z")
 
+cmap = ListedColormap(sns.color_palette("hls", n_colors=12))
+sc = ax.scatter(x, y, z, s=25, c=c, marker='o', cmap=cmap)
+plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=7)
+plt.show()
+
+
 # get colormap from seaborn see:
 # https://stackoverflow.com/questions/52285104/3d-scatterplots-in-python-with-hue-colormap-and-legend
-cmap = ListedColormap(sns.color_palette("hls", n_colors=12))
-
-# draw scatter plot
-#ax.scatter(x, y, z)
-
-# plot
-sc = ax.scatter(x, y, z, s=25, c=c, marker='o', cmap=cmap)
-
-# legend
-plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=7)
-
-
-# show scatter plot
-plt.show()
