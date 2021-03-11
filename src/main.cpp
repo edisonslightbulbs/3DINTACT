9594e3cc860eb791daa682e286bd28a2efd19c32
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-11 16:20:30 +0100

    changing to submodule strategy

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,41 @@
 #include <vector>
 
-#include "dbscan.h"
+//#include "dbscan.h"
 #include "io.h"
 #include "knn.h"
 #include "logger.h"
 #include "point.h"
+#include "lda.h"
 #include "timer.h"
 
 
 int main(int argc, char* argv[])
 {
+
     /** initialize logger */
     logger(argc, argv);
 
-    /** initialize point cloud container */
+    /** point cloud */
     std::vector<Point> points;
+    const std::string DATA = io::pwd() + "/resources/inputdata.ply";
+    points = io::read(points, DATA.c_str());
 
-    /** read data */
-    const std::string DATA = IO::pwd() + "/resources/inputdata.ply";
-    points = IO::read(points, DATA.c_str());
+    /** perform linear discriminant analysis */
+    points = lda::reduce(points); // <-- ~ 59 ms O(N^2)
 
     /** estimate epsilon neighbourhood */
-    float epsilon = knn::elbow(points);
-    std::cout << epsilon << std::endl;
+    {
+        Timer timer;
+        float epsilon = knn::elbow(points); // <-- best time 292 ms O(N^2)
+        std::cout << epsilon << std::endl;
+        LOG(INFO) << timer.getDuration() << "ms : knn analysis";
+    }
 
     /** cluster using dbscan */
     // std::vector<Point> clusteredPoints;
     // clusteredPoints = dbscan::cluster(points, MIN_POINTS);
     // IO::write(clusteredPoints, IO::pwd() + "/build/results.csv");
+
+
     return 0;
 }
