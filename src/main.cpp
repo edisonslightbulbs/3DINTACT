0328b4fd8d14fdca78d575dc313785ace531c047
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-14 12:34:11 +0100

    improving interface learnability

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@
 #include "point.h"
 #include "lda.h"
 #include "timer.h"
-
+#include "seed.h"
+#include "elbow.h"
+#include "iqr.h"
 
 int main(int argc, char* argv[])
 {
-
     /** initialize logger */
     logger(argc, argv);
 
@@ -20,16 +21,24 @@ int main(int argc, char* argv[])
     const std::string DATA = io::pwd() + "/resources/inputdata.ply";
     points = io::read(points, DATA.c_str());
 
-    /** perform linear discriminant analysis */
-    points = lda::reduce(points); // <-- ~ 59 ms O(N^2)
+    /** linear discriminant analysis: dimension reduction */
+    points = lda::reduce(points);
+
+    /** outlier removal */
+    points = iqr::denoise(points);
+
+    /** sample */
+    std::vector<Point> sample(seed::sample(points));
+
+    /** query sample distances to the 4th nearest neighbours */
+    std::vector<float> knn4 = knn::compute(sample);
 
     /** estimate epsilon neighbourhood */
-    {
-        Timer timer;
-        float epsilon = knn::elbow(points); // <-- best time 292 ms O(N^2)
-        std::cout << epsilon << std::endl;
-        //LOG(INFO) << timer.getDuration() << "ms : knn analysis";
-    }
+    float epsilon = elbow::find(knn4); // <-- best time 292 ms O(N^2)
+    std::cout << epsilon << std::endl;
+
+
+
 
     /** cluster using dbscan */
     // std::vector<Point> clusteredPoints;
