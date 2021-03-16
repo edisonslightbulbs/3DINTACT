7716fd58922eba4623082dcf9a605dfd1a7f7a05
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-16 16:21:51 +0100

    beta testing a geometric processing approach

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #include "lda.h"
 #include "sampler.h"
 #include "elbow.h"
-#include "iqr.h"
 #include "svd.h"
+#include "area.h"
 #include "segmenter.h"
 
 extern const int FAIL = -3;
@@ -18,41 +18,41 @@ const int tweak = 60;
 static std::vector<Point> segment(std::vector<Point> points){
     Timer timer;
     /** reduce dimensions  */
-    std::vector<Point> analyzedPoints = lda::reduce(points);
-
-    /** remove outliers */
-    std::vector<Point> denoisedPoints = iqr::denoise(analyzedPoints);
+    std::vector<Point> reducedPoints = lda::reduce(points);
 
     /** grow region of interest */
-    std::vector<Point> roi = svd::compute(denoisedPoints);
+    std::vector<Point> roi = svd::compute(reducedPoints);
+
+    /** define tabletop area */
+    std::vector<Point> area = area::estimate(roi);
 
     /** sample grown region */
     std::vector<Point> sample = sampler::sample(roi);
 
-    /** accumulate distances to 4th nearest neighbours */
-    std::vector<float> knn4 = knn::compute(sample);
+    // /** accumulate distances to 4th nearest neighbours */
+    // std::vector<float> knn4 = knn::compute(sample);
 
-    /** find epsilon queryRange size */
-    float epsilon = elbow::find(knn4);
-    LOG(INFO) << epsilon << ": ≈ epsilon";
+    // /** find epsilon queryRange size */
+    // float epsilon = elbow::find(knn4);
+    // LOG(INFO) << epsilon << ": ≈ epsilon";
 
-    /** run: return clustered points and number of clusters */
-    const int MIN_POINTS = 4; // <-- https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.121.9220
-    std::pair<std::vector<Point>, int> clusters = dbscan::cluster(roi, MIN_POINTS, epsilon + tweak);
+    // /** run: return clustered points and number of clusters */
+    // const int MIN_POINTS = 4; // <-- https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.121.9220
+    // std::pair<std::vector<Point>, int> clusters = dbscan::cluster(roi, MIN_POINTS, epsilon + tweak);
 
-    /** segment tabletop interaction context */
-    std::vector<Point> context = segmenter::segment(clusters);
+    // /** segment tabletop interaction context */
+    // std::vector<Point> context = segmenter::segment(clusters);
 
     LOG(INFO) << "..........................................................";
     LOG(INFO) << timer.getDuration() << " ms: total segmentation runtime (!unoptimized!)";
 
-    /** external resources for downstream analysis */
-    io::log_performance(points.size(), denoisedPoints.size(), roi.size(), context.size(), timer.getDuration());
-    io::write_ply(context);
+    // /** external resources for downstream analysis */
+    // io::log_performance(points.size(), denoisedPoints.size(), roi.size(), context.size(), timer.getDuration());
+    // io::write_ply(context);
 
-    return context;
+    //return context;
+    return roi;
 }
-
 
 int main(int argc, char* argv[])
 {
@@ -65,7 +65,7 @@ int main(int argc, char* argv[])
     points = io::read(points, DATA.c_str());
 
     /** segment tabletop interaction context */
-    std::vector<Point> tic = segment(points);
+    std::vector<Point> context = segment(points);
 
     return PASS;
 }
