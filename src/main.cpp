4b7169edaaad921e132bed27fe68f2b73008dd6a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-19 13:04:42 +0100

    adapting main interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <vector>
 
+#include "frame.h"
 #include "io.h"
+#include "kinect.h"
 #include "lda.h"
 #include "point.h"
 #include "scalpel.h"
@@ -8,6 +10,8 @@
 
 extern const int FAIL = -3;
 extern const int PASS = 0;
+
+const float TO_PERCENTAGE = 100;
 
 static std::vector<Point> segment(std::vector<Point> points)
 {
@@ -24,12 +28,10 @@ static std::vector<Point> segment(std::vector<Point> points)
     std::vector<Point> context = scalpel::segment(roi);
     std::string segmentTime = timer.getDuration();
 
-    LOG(INFO) << timer.getDuration() << " ms: total runtime";
-
-    float percent = 100;
-    float refineDataReduction = (float)refined.size() / points.size() * percent;
-    float roiDataReduction = (float)roi.size() / points.size() * percent;
-    float segmentDataReduction = (float)context.size() / points.size();
+    /** readable queries */
+    float refineDataReduction = (float)refined.size() / points.size() * TO_PERCENTAGE;
+    float roiDataReduction = (float)roi.size() / points.size() * TO_PERCENTAGE;
+    float segmentDataReduction = (float)context.size() / points.size() *TO_PERCENTAGE;
 
     /** output resources for downstream analysis */
     io::performance(points.size(), refined.size(), refineTime, roi.size(),
@@ -39,18 +41,40 @@ static std::vector<Point> segment(std::vector<Point> points)
     return context;
 }
 
+static std::vector<Point> plyFile()
+{
+    std::vector<Point> points;
+    const std::string DATA = io::pwd() + "/output/tabletop.ply";
+    points = io::read(points, DATA.c_str());
+    return points;
+}
+
+static std::vector<Point> image()
+{
+    Kinect kinect;                   // <- set up kinect
+    Frame frame = kinect.getImage(); // <- get surface view
+    frame.release();                 // <- release resources
+    kinect.close();
+
+    // frame.m_rgb;                  // <- synchronized rgb image
+    // frame.m_points;               // <- point cloud
+
+    return frame.m_points;
+}
+
 int main(int argc, char* argv[])
 {
     /** initialize logger */
     logger(argc, argv);
 
-    /** developer testing point cloud */
-    std::vector<Point> points;
-    const std::string DATA = io::pwd() + "/output/tabletop.ply";
-    points = io::read(points, DATA.c_str());
+    /** get point cloud */
+    //std::vector<Point> points = plyFile();
+    std::vector<Point> points = image();
 
+    Timer timer;
     /** segment tabletop interaction context */
     std::vector<Point> context = segment(points);
+    LOG(INFO) << timer.getDuration() << " ms: segmentation runtime";
 
     /** output point cloud as a *.ply file */
     io::write_ply(context);
