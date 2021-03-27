73323cf11e23848c16bf485d6b3e38beda14b8f5
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-27 14:59:20 +0100

    cleaning main before extending interface for real-time-rendering

--- src/main.cpp
+++ src/main.cpp
@@ -10,50 +10,20 @@
 extern const int FAIL = -3;
 extern const int PASS = 0;
 
-static std::vector<Point> plyFile()
+int main(int argc, char* argv[])
 {
-    std::vector<Point> points;
-    const std::string DATA = io::pwd() + "/output/tabletop.ply";
-    points = io::read(points, DATA.c_str());
-    return points;
-}
-
-/** Iff on mac, emulate kinect using resource in ./output/ */
-#if __linux__
-static std::vector<Point> getKinectImage()
-{
+    /** initialize logger */
+    logger(argc, argv);
     Kinect kinect;                   // <- set up kinect
     Frame frame = kinect.getImage(); // <- get surface view
     io::write(frame.m_rgb);          // <- synchronized rgb image
     frame.release();                 // <- release resources
     kinect.close();                  // <- close kinect
-    return frame.m_points;           // <- point cloud
-}
-#endif
-
-int main(int argc, char* argv[])
-{
-    /** initialize logger */
-    logger(argc, argv);
-
-    /** get point cloud */
-    std::vector<Point> points;
-
-/** Iff on mac, emulate kinect using resource in ./output/ */
-#if __linux__
-    points = getKinectImage();
-#elif __APPLE__
-    points = plyFile();
-#endif
 
     /** segment tabletop interaction context */
-    Timer timer;
-    std::vector<Point> context = segment::cut(points);
-    LOG(INFO) << "Interaction context segmented in: " << timer.getDuration()
-              << " ms";
+    std::vector<Point> context = segment::cut(frame.m_points);
 
-    /** write point cloud to ./output/context.ply file */
-    io::ply(points, context); // <- superimpose colorized context of original raw point cloud
-    //io::ply( context); // <- isolated segment
+    /** output and visualize segment */
+    io::ply(frame.m_points, context);
     return PASS;
 }
