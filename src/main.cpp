b0fd245b3ed6314fa61c1f5abb5084aa08bfdab4
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-27 22:45:09 +0100

    cleaning main interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,14 @@
-#include <vector>
 
-#include "frame.h"
-#include "io.h"
 #include "kinect.h"
-#include "logger.h"
-#include "point.h"
-#include "segment.h"
+#include "workers.h"
 
 extern const int FAIL = -3;
 extern const int PASS = 0;
 
 int main(int argc, char* argv[])
 {
-    /** initialize logger */
     logger(argc, argv);
-    Kinect kinect;                   // <- set up kinect
-    Frame frame = kinect.getImage(); // <- get surface view
-    io::write(frame.m_rgb);          // <- synchronized rgb image
-    frame.release();                 // <- release resources
-    kinect.close();                  // <- close kinect
-
-    /** segment tabletop interaction context */
-    std::vector<Point> context = segment::cut(frame.m_points);
-
-    /** output and visualize segment */
-    io::ply(frame.m_points, context);
+    Kinect kinect;
+    workers::manage(kinect);
     return PASS;
 }
