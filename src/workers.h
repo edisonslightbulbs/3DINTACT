b948848b99a71b55718b25f27cb735981da2b95c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-28 16:26:36 +0200

    dirty implementation of real-time rendering of segmented tabletop interaction context

--- src/workers.h
+++ src/workers.h
@@ -13,62 +13,29 @@
 #include "logger.h"
 #include "point.h"
 #include "renderer.h"
-#include "segment.h"
 #include "timer.h"
 
 namespace workers {
 
-/** In theory this thread will execute every 30 ms to assess the tabletop
- *  context for down stream processing.
- */
-void context(Kinect kinect)
-{
-    std::vector<Point> context = segment::cut(kinect.m_points);
 
-    std::vector<float> x;
-    std::vector<float> y;
-    std::vector<float> z;
+    void manage(Kinect& kinect)
+    {
+        /** MUTEX NOT IN USE ... YET */
+        std::mutex m;
 
-    for (const auto& point : kinect.m_points) {
-        x.push_back(point.m_x);
-        y.push_back(point.m_y);
-        z.push_back(point.m_z);
+        const int KINECT_RESOLUTION = 640 * 576;
+        auto sptr_points = std::make_shared<std::vector<float>>(KINECT_RESOLUTION * 3);
+
+        /** render in separate worker thread */
+        std::thread rendering(renderer::render, std::ref(m), std::ref(kinect),
+            std::ref(KINECT_RESOLUTION), std::ref(sptr_points));
+
+        /** manage worker threads */
+        rendering.join();
+
+        /** release and close kinect */
+        kinect.release();
+        kinect.close();
     }
-    float xMax = *std::max_element(x.begin(), x.end());
-    float xMin = *std::min_element(x.begin(), x.end());
-    float yMax = *std::max_element(y.begin(), y.end());
-    float yMin = *std::min_element(y.begin(), y.end());
-    float zMax = *std::max_element(z.begin(), z.end());
-    float zMin = *std::min_element(z.begin(), z.end());
-
-    Point maxPoint(xMax, yMax, zMax);
-    Point minPoint(xMin, yMin, zMin);
-
-    io::ply(kinect.m_points, context);
-}
-
-void manage(const Kinect& kinect)
-{
-    /** MUTEX NOT IN USE ... YET */
-    std::mutex m;
-
-    int pointNum = 640 * 576;
-    auto sptr_points = std::make_shared<std::vector<float>>(pointNum * 3);
-
-    /** preprocess in separate thread */
-    std::thread preprocessing(context, std::ref(kinect));
-
-    /** render in separate thread */
-    std::thread rendering(renderer::render, std::ref(m), std::ref(kinect),
-        std::ref(pointNum), std::ref(sptr_points));
-
-    /** manage thread handles */
-    preprocessing.join();
-    rendering.join();
-
-    /** release and close kinect */
-    kinect.release();
-    kinect.close();
-}
 }
 #endif /* WORKERS_H */
