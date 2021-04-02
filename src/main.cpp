00f3a663e441f1064c16c9196af2f9a94cf50d7c
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-02 11:37:25 +0200

    improving the readability of the main interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <mutex>
 #include <thread>
-#include <vector>
 
 #include "kinect.h"
 #include "intact.h"
@@ -10,33 +8,15 @@ extern const int PASS = 0;
 
 void work(Kinect& kinect)
 {
-    /** TODO: */
-    std::mutex m;
-
-    Point maxConstraint;
-    Point minConstraint;
-    std::pair<Point, Point> threshold(minConstraint, maxConstraint);
-    const int KINECT_RESOLUTION = 640 * 576;
-
-    /** shared pointers */
-    auto sptr_points = std::make_shared<std::vector<float>>(KINECT_RESOLUTION * 3);
-    auto sptr_threshold = std::make_shared<std::pair<Point, Point>>(threshold);
-
     /** segment in separate worker thread */
-    std::thread segmenting(intact::segment, std::ref(m), std::ref(kinect),
-                          std::ref(KINECT_RESOLUTION), std::ref(sptr_points), std::ref(sptr_threshold));
+    std::thread segmenting(intact::segment, std::ref(kinect));
 
     /** render in separate worker thread */
-    std::thread rendering(intact::render, std::ref(m), std::ref(kinect),
-                          std::ref(KINECT_RESOLUTION), std::ref(sptr_points), std::ref(sptr_threshold));
+    std::thread rendering(intact::render, std::ref(kinect));
 
     /** manage worker threads */
     segmenting.join();
     rendering.join();
-
-    /** release and close kinect */
-    kinect.release();
-    kinect.close();
 }
 
 int main(int argc, char* argv[])
