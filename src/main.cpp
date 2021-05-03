8397998ce3f45759653a09189b15e317097d6aac
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-03 17:07:39 +0200

    moving point cloud capture and filter implementation to main interface

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,105 @@
 void sense(
     std::shared_ptr<Kinect>& sptr_kinect, std::shared_ptr<Intact>& sptr_intact)
 {
+    // Here is the interesting bit:
+    // Your sensor of choice (which needn't be the Kinect) should
+    // handshake 3DINTACT's API. No adapter is provided for
+    // casting different point-cloud data structures into the
+    // structure required by the API. This has to be done manually
+    // per specific case. The only important thing to note is the
+    // API requires data two vectors data structures:
+    //   1) std::vector<float> points;
+    //      where points =
+    //      { x_0, y_0, z_0, x_1, y_1, z_1, ..., x_n, y_n, z_n }
+    //   2) std::vector<uint8_t> color;
+    //      where color =
+    //      { r_0, g_0, b_0, r_1, g_1, b_1, ..., r_n, g_n, b_n }
+    //
     bool init = true;
     while (sptr_intact->isRun()) {
-        /** get next frame: */
+
+        /** capture point cloud */
         sptr_kinect->getFrame(RGB_TO_DEPTH);
-        sptr_intact->buildPcl(
-            sptr_kinect->getPclImage(), sptr_kinect->getRgb2DepthImage());
+
+        /** hand over point cloud to API */
+        auto* data
+            = (int16_t*)(void*)k4a_image_get_buffer(sptr_kinect->getPclImage());
+        uint8_t* color = k4a_image_get_buffer(sptr_kinect->getRgb2DepthImage());
+
+        const int N = sptr_intact->getNumPoints();
+
+        std::vector<float> raw(N * 3);
+        std::vector<uint8_t> rawColor(N * 3);
+
+        std::vector<float> segment(N * 3);
+        std::vector<uint8_t> segmentColor(N * 3);
+
+        std::vector<float> region(N * 3);
+        std::vector<uint8_t> regionColor(N * 3);
+
+        /** n.b., kinect colors reversed! */
+        for (int i = 0; i < N; i++) {
+            if (data[3 * i + 2] == 0) {
+                raw[3 * i + 0] = 0.0f;
+                raw[3 * i + 1] = 0.0f;
+                raw[3 * i + 2] = 0.0f;
+                rawColor[3 * i + 2] = color[4 * i + 0];
+                rawColor[3 * i + 1] = color[4 * i + 1];
+                rawColor[3 * i + 0] = color[4 * i + 2];
+                continue;
+            }
+            raw[3 * i + 0] = (float)data[3 * i + 0];
+            raw[3 * i + 1] = (float)data[3 * i + 1];
+            raw[3 * i + 2] = (float)data[3 * i + 2];
+            rawColor[3 * i + 2] = color[4 * i + 0];
+            rawColor[3 * i + 1] = color[4 * i + 1];
+            rawColor[3 * i + 0] = color[4 * i + 2];
+
+            /** segment interaction regions  */
+            if (sptr_intact->getSegmentBoundary().second.m_xyz[2] == __FLT_MAX__
+                || sptr_intact->getSegmentBoundary().first.m_xyz[2]
+                    == __FLT_MIN__) {
+                continue;
+            }
+
+            if ((float)data[3 * i + 0]
+                    > sptr_intact->getSegmentBoundary().second.m_xyz[0]
+                || (float)data[3 * i + 0]
+                    < sptr_intact->getSegmentBoundary().first.m_xyz[0]
+                || (float)data[3 * i + 1]
+                    > sptr_intact->getSegmentBoundary().second.m_xyz[1]
+                || (float)data[3 * i + 1]
+                    < sptr_intact->getSegmentBoundary().first.m_xyz[1]
+                || (float)data[3 * i + 2]
+                    > sptr_intact->getSegmentBoundary().second.m_xyz[2]
+                || (float)data[3 * i + 2]
+                    < sptr_intact->getSegmentBoundary().first.m_xyz[2]) {
+                continue;
+            }
+            segment[3 * i + 0] = (float)data[3 * i + 0];
+            segment[3 * i + 1] = (float)data[3 * i + 1];
+            segment[3 * i + 2] = (float)data[3 * i + 2];
+            segmentColor[3 * i + 2] = color[4 * i + 0];
+            segmentColor[3 * i + 1] = color[4 * i + 1];
+            segmentColor[3 * i + 0] = color[4 * i + 2];
+        }
+        sptr_intact->setRaw(raw);
+        sptr_intact->setRawColor(rawColor);
+
+        if (sptr_intact->getSegmentBoundary().second.m_xyz[2] == __FLT_MAX__
+            || sptr_intact->getSegmentBoundary().first.m_xyz[2]
+                == __FLT_MIN__) {
+            sptr_intact->setSegment(raw);
+            sptr_intact->setSegmentColor(rawColor);
+        } else {
+            sptr_intact->setSegment(segment);
+            sptr_intact->setSegmentColor(segmentColor);
+        }
+
+        /** release kinect resources after each frame */
         sptr_kinect->release();
 
-        /** inform threads: init frame processing done */
+        /** update initialization semaphore */
         if (init) {
             init = false;
             sptr_intact->raiseKinectReadyFlag();
@@ -71,22 +161,8 @@ int main(int argc, char* argv[])
     sptr_intact->raiseRunFlag();
 
     /** start sensing */
-    std::thread senseWorker(sense, std::ref(sptr_kinect), std::ref(sptr_intact));
-    // Here is the interesting bit:
-    // Your sensor of choice (which needn't be the Kinect) should
-    // handshake 3DINTACT's API. No adaptors are provided
-    // casting different point cloud data structures into the
-    // structure required by the API. This is something that
-    // has to be done manually for each specific point-cloud
-    // source. The only important thing to note is point-cloud
-    // data input to the API comprises of two vectors,
-    //   i) std::vector<float> points;
-    //      where points =
-    //      { x_0, y_0, z_0, x_1, y_1, z_1, ... }
-    //  ii) std::vector<uint8_t> color;
-    //      where color =
-    //      { r_0, g_0, b_0, r_1, g_1, b_1, ... }
-
+    std::thread senseWorker(
+        sense, std::ref(sptr_kinect), std::ref(sptr_intact));
 
     /** sense */
     std::thread calibrateWorker(calibrate, std::ref(sptr_intact));
