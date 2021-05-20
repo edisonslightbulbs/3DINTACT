ceba82d93d2e81e8cec97374658e4d0c18fa7a94
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-20 17:27:04 +0200

    migrating helper functions

--- src/helpers.hpp
+++ src/helpers.hpp
@@ -67,13 +67,6 @@ void zeroPixel(const int& index, std::vector<uint8_t>& img)
     img[3 * index + 0] = 0;
 }
 
-void chromaPixel(const int& index, uint8_t* ptr_data)
-{
-    ptr_data[4 * index + 0] = 93;  // blue
-    ptr_data[4 * index + 1] = 171; // green
-    ptr_data[4 * index + 2] = 65;  // red
-    ptr_data[4 * index + 3] = 0;   // alpha
-}
 
 void updateSegment(std::shared_ptr<Intact>& sptr_intact,
     const std::vector<float>& pcl, const std::vector<uint8_t>& img,
@@ -107,20 +100,4 @@ bool inSegment(
     return true;
 }
 
-bool vacant(
-    const int& index, const short* ptr_data, const Point& min, const Point& max)
-{
-    if (max.m_xyz[2] == __FLT_MAX__ || min.m_xyz[2] == __FLT_MIN__) {
-        return false;
-    }
-    if ((float)ptr_data[3 * index + 0] > max.m_xyz[0]
-        || (float)ptr_data[3 * index + 0] < min.m_xyz[0]
-        || (float)ptr_data[3 * index + 1] > max.m_xyz[1]
-        || (float)ptr_data[3 * index + 1] < min.m_xyz[1]
-        || (float)ptr_data[3 * index + 2] > max.m_xyz[2]
-        || (float)ptr_data[3 * index + 2] < min.m_xyz[2]) {
-        return false;
-    }
-    return true;
-}
 #endif /*HELPERS_H */
