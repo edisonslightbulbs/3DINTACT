1d86ef82ce83fc9b74a8076940bf29c7e318ccb9
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-27 15:30:35 +0200

    revising main client interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <mutex>
 #include <string>
 #include <thread>
 #include <torch/script.h>
@@ -7,132 +8,136 @@
 #include "intact.h"
 #include "io.h"
 #include "kinect.h"
-#include "logger.h"
 #include "macros.hpp"
 
-void render(std::shared_ptr<Intact>& sptr_intact)
-{
-    sptr_intact->render(sptr_intact);
-}
+std::mutex m;
+
+void render(std::shared_ptr<Intact>& sptr_i3d) { sptr_i3d->render(sptr_i3d); }
 
 void detect(std::shared_ptr<Intact>& sptr_intact)
 {
     std::vector<std::string> classNames;
     torch::jit::script::Module module;
     i3d::configTorch(classNames, module);
-    sptr_intact->showObjects(classNames, module, sptr_intact);
+    sptr_intact->findObjects(classNames, module, sptr_intact);
 }
 
-void segment(std::shared_ptr<Intact>& sptr_intact)
+void findRegion(std::shared_ptr<Intact>& sptr_i3d)
 {
-    sptr_intact->segment(sptr_intact);
+    sptr_i3d->region(sptr_i3d);
 }
 
-void sift(std::shared_ptr<Intact>& sptr_intact)
+void segment(std::shared_ptr<Intact>& sptr_i3d) { sptr_i3d->segment(sptr_i3d); }
+
+void frame(std::shared_ptr<Intact>& sptr_i3d) { sptr_i3d->frame(sptr_i3d); }
+
+void chromakey(std::shared_ptr<Intact>& sptr_i3d)
 {
-    sptr_intact->sift(sptr_intact);
+    sptr_i3d->chromakey(sptr_i3d);
 }
 
-void chromakey(std::shared_ptr<Intact>& sptr_intact)
-{
-    sptr_intact->chromakey(sptr_intact);
-}
-
-void cluster(std::shared_ptr<Intact>& sptr_intact)
+void cluster(std::shared_ptr<Intact>& sptr_i3d)
 {
     int minPoints = 4;
     const float epsilon = 3.170;
-    sptr_intact->cluster(epsilon, minPoints, sptr_intact);
+    sptr_i3d->cluster(epsilon, minPoints, sptr_i3d);
+}
+
+void buildPcl(std::shared_ptr<Intact>& sptr_i3d)
+{
+    sptr_i3d->buildPCloud(sptr_i3d);
 }
 
 void k4aCapture(
-    std::shared_ptr<Kinect>& sptr_kinect, std::shared_ptr<Intact>& sptr_intact)
+    std::shared_ptr<Kinect>& sptr_kinect, std::shared_ptr<Intact>& sptr_i3d)
 {
-    int numPts = sptr_intact->m_numPoints;
-    int pclsize = sptr_intact->m_pclsize;
-    int imgsize = sptr_intact->m_imgsize;
-
-    int16_t pclBuf[pclsize];
-    uint8_t imgBuf_GL[pclsize];
-    uint8_t imgBuf_CV[imgsize];
-
     START
-    while (sptr_intact->isRun()) {
-        sptr_kinect->getFrame(RGB_TO_DEPTH);
+    sptr_kinect->capture();
+    sptr_kinect->depthCapture();
+    int w = k4a_image_get_width_pixels(sptr_kinect->m_depth);
+    int h = k4a_image_get_height_pixels(sptr_kinect->m_depth);
 
-        auto* ptr_pcl
-            = (int16_t*)(void*)k4a_image_get_buffer(sptr_kinect->getPcl());
-        auto* ptr_img = k4a_image_get_buffer(sptr_kinect->getRgb2DepthImg());
-        int width = k4a_image_get_width_pixels(sptr_kinect->getDepthImg());
-        int height = k4a_image_get_height_pixels(sptr_kinect->getDepthImg());
+    while (sptr_i3d->isRun()) {
+        sptr_kinect->capture();
+        sptr_kinect->depthCapture();
+        sptr_kinect->pclCapture();
+        sptr_kinect->imgCapture();
+        sptr_kinect->c2dCapture();
+        sptr_kinect->transform(RGB_TO_DEPTH);
 
-        std::vector<Point> refinedPoints;
-        std::vector<Point> unrefinedPoints;
-        for (int i = 0; i < numPts; i++) {
-            Point point;
-            i3d::addPoint(i, pclBuf, ptr_pcl);
-            i3d::addPixel_GL(i, imgBuf_GL, ptr_img);
-            i3d::addPixel_CV(i, imgBuf_CV, ptr_img);
-            i3d::adapt(i, point, pclBuf, imgBuf_CV);
-            unrefinedPoints.emplace_back(point);
+        auto* ptr_k4aImgData = k4a_image_get_buffer(sptr_kinect->m_c2d);
+        auto* ptr_k4aPCloudData
+            = (int16_t*)(void*)k4a_image_get_buffer(sptr_kinect->m_pcl);
+        auto* ptr_k4aDepthData
+            = (uint16_t*)(void*)k4a_image_get_buffer(sptr_kinect->m_depth);
+        auto* ptr_k4aTableData
+            = (k4a_float2_t*)(void*)k4a_image_get_buffer(sptr_kinect->m_xyT);
 
-            if (ptr_pcl[3 * i + 2] == 0) {
-                continue;
-            }
-            refinedPoints.emplace_back(point);
-        }
-        sptr_kinect->release();
-        sptr_intact->setDepthImgWidth(width);
-        sptr_intact->setDepthImgHeight(height);
-        sptr_intact->setSensorPcl(pclBuf);
-        sptr_intact->setSensorImg_GL(imgBuf_GL);
-        sptr_intact->setSensorImg_CV(imgBuf_CV);
-        sptr_intact->setRefinedPoints(refinedPoints);
-        sptr_intact->setUnrefinedPoints(unrefinedPoints);
-        KINECT_READY
-        POLLING_EXIT_STATUS
+        // share k4a resources with intact
+        sptr_i3d->setDepthWidth(w);
+        sptr_i3d->setDepthHeight(h);
+        sptr_i3d->setSensorImgData(ptr_k4aImgData);
+        sptr_i3d->setSensorTableData(ptr_k4aTableData);
+        sptr_i3d->setSensorDepthData(ptr_k4aDepthData);
+        sptr_i3d->setSensorPCloudData(ptr_k4aPCloudData);
+
+        // release k4a resources
+        sptr_kinect->releaseK4aImages();
+        sptr_kinect->releaseK4aCapture();
+        RAISE_SENSOR_RESOURCES_READY_FLAG
+        POLL_EXIT_STATUS
     }
 }
 
 int main(int argc, char* argv[])
 {
-    std::cout << "-- press ESC to exit." << std::endl;
     logger(argc, argv);
+    LOG(INFO) << "-- 3DINTACT is currently unstable and should only be used "
+                 "for academic purposes!";
+    LOG(INFO) << "-- press ESC to exit";
 
     // initialize kinect
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
 
     // initialize the 3dintact API
-    std::shared_ptr<Intact> sptr_intact(new Intact(sptr_kinect->m_numPts));
-    sptr_intact->raiseRunFlag();
+    std::shared_ptr<Intact> sptr_i3d(new Intact());
+    sptr_i3d->raiseRunFlag();
 
     // capture using depth sensor
     std::thread k4aCaptureWorker(
-        k4aCapture, std::ref(sptr_kinect), std::ref(sptr_intact));
+        k4aCapture, std::ref(sptr_kinect), std::ref(sptr_i3d));
 
-    // render
-    std::thread renderWorker(render, std::ref(sptr_intact));
+    // build point cloud
+    std::thread buildPCloudWorker(buildPcl, std::ref(sptr_i3d));
 
-    // detect objects
-    std::thread detectWorker(detect, std::ref(sptr_intact));
+    // find region of interest
+    std::thread findRegionWorker(findRegion, std::ref(sptr_i3d));
+
+    // create GL and CV specific frames
+    std::thread frameWorker(frame, std::ref(sptr_i3d));
 
     // segment
-    std::thread segmentWorker(segment, std::ref(sptr_intact));
+    std::thread segmentWorker(segment, std::ref(sptr_i3d));
 
-    // sift segment
-    std::thread siftWorker(sift, std::ref(sptr_intact));
+    // render
+    std::thread renderWorker(render, std::ref(sptr_i3d));
+
+    // find objects
+    std::thread detectWorker(detect, std::ref(sptr_i3d));
 
     // cluster
-    std::thread clusterWorker(cluster, std::ref(sptr_intact));
+    std::thread clusterWorker(cluster, std::ref(sptr_i3d));
 
     /* example: chroma-keying the tabletop surface */
-    std::thread chromakeyWorker(chromakey, std::ref(sptr_intact));
+    std::thread chromakeyWorker(chromakey, std::ref(sptr_i3d));
 
     k4aCaptureWorker.join();
+    buildPCloudWorker.join();
     renderWorker.join();
     detectWorker.join();
+    findRegionWorker.join();
     segmentWorker.join();
-    siftWorker.join();
+    frameWorker.join();
     clusterWorker.join();
     chromakeyWorker.join();
 
