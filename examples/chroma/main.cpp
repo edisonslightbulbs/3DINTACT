5c5c2c6eaf562eed571461ce1a4d6e786886149f
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-29 10:39:56 +0200

    working on chroma example

--- examples/chroma/main.cpp
+++ examples/chroma/main.cpp
@@ -1,6 +1,5 @@
 #include <Eigen/Dense>
 #include <chrono>
-#include <mutex>
 #include <string>
 #include <thread>
 
@@ -11,25 +10,21 @@
 #include "macros.hpp"
 #include "svd.h"
 
-std::mutex m;
-
-void findRegion(std::shared_ptr<i3d>& sptr_i3d)
-{
-    sptr_i3d->proposeRegion(sptr_i3d);
-}
-
-void segment(std::shared_ptr<i3d>& sptr_i3d)
-{
-    sptr_i3d->segmentRegion(sptr_i3d);
-}
-
-void frame(std::shared_ptr<i3d>& sptr_i3d) { sptr_i3d->frameRegion(sptr_i3d); }
-
-void cluster(std::shared_ptr<i3d>& sptr_i3d)
+void clusterRegion(std::shared_ptr<i3d>& sptr_i3d)
 {
     int minPoints = 4;
     const float epsilon = 3.170;
     sptr_i3d->clusterRegion(epsilon, minPoints, sptr_i3d);
+}
+
+void segmentRegion(std::shared_ptr<i3d>& sptr_i3d)
+{
+    sptr_i3d->segmentRegion(sptr_i3d);
+}
+
+void proposeRegion(std::shared_ptr<i3d>& sptr_i3d)
+{
+    sptr_i3d->proposeRegion(sptr_i3d);
 }
 
 void buildPcl(std::shared_ptr<i3d>& sptr_i3d)
@@ -87,132 +82,124 @@ int main(int argc, char* argv[])
                  "for academic purposes!";
     LOG(INFO) << "-- press ESC to exit";
 
-    // initialize kinect
+    // initialize k4a kinect
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
 
-    // initialize the 3dintact API
+    // initialize the 3dintact
     std::shared_ptr<i3d> sptr_i3d(new i3d());
     sptr_i3d->raiseRunFlag();
 
-    // capture using depth sensor
+    // capture using k4a depth sensor
     std::thread k4aCaptureWorker(
         k4aCapture, std::ref(sptr_kinect), std::ref(sptr_i3d));
 
     // build point cloud
     std::thread buildPCloudWorker(buildPcl, std::ref(sptr_i3d));
 
-    // find region of interest
-    std::thread findRegionWorker(findRegion, std::ref(sptr_i3d));
+    // propose region
+    std::thread proposeRegionWorker(proposeRegion, std::ref(sptr_i3d));
 
-    // create GL and CV specific frames
-    std::thread frameRegionWorker(frame, std::ref(sptr_i3d));
+    // segment region
+    std::thread segmentRegionWorker(segmentRegion, std::ref(sptr_i3d));
 
-    // segment
-    std::thread segmentRegionWorker(segment, std::ref(sptr_i3d));
+    // cluster segmented region
+    std::thread clusterRegionWorker(clusterRegion, std::ref(sptr_i3d));
 
-    // cluster
-    std::thread clusterRegionWorker(cluster, std::ref(sptr_i3d));
+    // ------> do stuff with tabletop environment <------
 
-    SLEEP_UNTIL_RESOURCES_READY
+    SLEEP_UNTIL_CLUSTERS_READY
     while (sptr_i3d->isRun()) {
-        // start frame rate clock
-        clock_t start = clock();
+        clock_t start = clock(); // <- frame-rate timestamp
+        auto clusters = sptr_i3d->getPCloudClusters(); // <-get clusters
+        auto indexClusters = clusters->second;
+        std::vector<Point> points = clusters->first;
 
-        // // get clusters
-        // auto clusters = sptr_i3d->getPCloudClusters();
-        // auto indexClusters = clusters->second;
-        // std::vector<Point> points = clusters->first;
+        // define chromakey color for tabletop surface
+        uint8_t rgb[3] = { chromagreen[0], chromagreen[1], chromagreen[2] };
+        uint8_t bgra[4] = { chromagreen[2], chromagreen[1], chromagreen[0], 0 };
 
-        // // define chromakey color for tabletop surface
-        // uint8_t rgb[3] = { chromagreen[0], chromagreen[1], chromagreen[2] };
-        // uint8_t bgra[4] = { chromagreen[2], chromagreen[1], chromagreen[0], 0
-        // };
+        // cast 'index clusters' to 'point clusters'
+        std::vector<std::vector<Point>> pointClusters(indexClusters.size());
+        for (int i = 0; i < indexClusters.size(); i++) {
+            std::vector<Point> heap(indexClusters[i].size());
+            for (int j = 0; j < indexClusters[i].size(); j++) {
+                heap[j] = points[indexClusters[i][j]];
+            }
+            pointClusters[i] = heap;
+        }
 
-        // // cast index clusters to clusters of points
-        // std::vector<std::vector<Point>> pointClusters(indexClusters.size());
-        // for (int i = 0; i < indexClusters.size() ; i ++) {
-        //     std::vector<Point> heap(indexClusters[i].size());
-        //     for (int j = 0; j < indexClusters[i].size(); j++) {
-        //         heap[j] = points[indexClusters[i][j]];
-        //     }
-        //     pointClusters[i]= heap;
-        // }
+        /** use SVD norm to find stitch vacant tabletop surface */
+        // config flags for svd computation
+        int flag = Eigen::ComputeThinU | Eigen::ComputeThinV;
 
-        // // config flags for svd computation
-        // int flag = Eigen::ComputeThinU | Eigen::ComputeThinV;
+        // heap norms for each cluster
+        std::vector<Eigen::Vector3d> normals(pointClusters.size());
+        for (int i = 0; i < pointClusters.size(); i++) {
+            SVD usv(pointClusters[i], flag);
+            normals[i] = usv.getV3Normal();
+        }
 
-        // // heap norms for each cluster
-        // std::vector<Eigen::Vector3d> normals(pointClusters.size());
-        // for(int i = 0; i < pointClusters.size(); i++){
-        //     SVD usv(pointClusters[i], flag);
-        //     normals[i] =  usv.getV3Normal();
+        // evaluate coplanarity between clusters
+        const float ARGMIN = 0.4;
+        int clusterIndex = 0;
+        std::vector<Point> tabletop = pointClusters[0];
+        for (const auto& normal : normals) {
+            double a = normals[0].dot(normal);
+            double b = normals[0].norm() * normal.norm();
+            double solution = std::acos(a / b);
 
-        // }
+            if (!std::isnan(solution) && solution < ARGMIN && solution > -ARGMIN
+                && pointClusters[clusterIndex].size() < 25) {
+                tabletop.insert(tabletop.end(),
+                    pointClusters[clusterIndex].begin(),
+                    pointClusters[clusterIndex].end());
+            }
+            clusterIndex++;
+        }
 
-        // const float ARGMIN = 0.4;
-        // // evaluate coplanarity between clusters
-        // int clusterIndex = 0;
-        // std::vector<Point> tabletop = pointClusters[0];
-        // for (const auto& normal : normals) {
-        //     double a = normals[0].dot(normal);
-        //     double b = normals[0].norm() * normal.norm();
-        //     double solution = std::acos(a / b);
+        // find the upper and lower z limits
+        std::vector<float> zMeasures(pointClusters.size());
+        for (int i = 0; i < pointClusters.size(); i++) {
+            zMeasures[i] = pointClusters[0][i].m_xyz[2];
+        }
+        int16_t maxH = *std::max_element(zMeasures.begin(), zMeasures.end());
+        int16_t minH = *std::min_element(zMeasures.begin(), zMeasures.end());
 
-        //     if (!std::isnan(solution) && solution < ARGMIN && solution >
-        //     -ARGMIN
-        //         && pointClusters[clusterIndex].size() < 25) {
-        //         tabletop.insert(tabletop.end(),
-        //                         pointClusters[clusterIndex].begin(),
-        //                         pointClusters[clusterIndex].end());
-        //     }
-        //     clusterIndex++;
-        // }
+        // use limits to refine tabletop points
+        std::vector<Point> bkgd; //
+        for (const auto& point : tabletop) {
+            if (point.m_xyz[2] > maxH || point.m_xyz[2] < minH) {
+                continue;
+            }
+            bkgd.emplace_back(point);
+        }
+        std::vector<Point> frame = *sptr_i3d->getPCloud();
 
-        // // find the upper and lower z limits
-        // std::vector<float> zMeasures(pointClusters.size());
-        // for(int i = 0; i < pointClusters.size(); i ++){
-        //     zMeasures[i] = pointClusters[0][i].m_xyz[2];
-
-        // }
-        // int16_t maxH = *std::max_element(zMeasures.begin(), zMeasures.end());
-        // int16_t minH = *std::min_element(zMeasures.begin(), zMeasures.end());
-
-        // // use limits to refine tabletop points
-        // std::vector<Point> bkgd; //
-        // for (const auto& point : tabletop) {
-        //     if (point.m_xyz[2] > maxH || point.m_xyz[2] < minH) {
-        //         continue;
-        //     }
-        //     bkgd.emplace_back(point);
-        // }
-        // std::vector<Point> frame = *sptr_i3d->getPCloud();
-
-        // // use clustered indexes to chromakey a selected cluster
-        // for (const auto& point : bkgd) {
-        //     int id = point.m_id;
-        //     frame[id].setPixel_GL(rgb);
-        //     frame[id].setPixel_CV(bgra);
-        // }
+        // use clustered indexes to chromakey a selected cluster
+        for (const auto& point : bkgd) {
+            int id = point.m_id;
+            frame[id].setPixel_GL(rgb);
+            frame[id].setPixel_CV(bgra);
+        }
         int w = sptr_i3d->getDepthWidth();
         int h = sptr_i3d->getDepthHeight();
 
-        // uint8_t imgBuf_CV[ w * h * 4];
+        uint8_t img_CV[w * h * 4];
+        for (int i = 0; i < w * h; i++) {
+            utils::stitch(i, frame[i], img_CV);
+        }
 
-        // for (int i = 0; i < w * h ; i++) {
-        //             utils::stitch(i, frame[i], imgBuf_CV);
-        //         }
+        cv::Mat img = cv::Mat(h, w, CV_8UC4, (void*)img_CV, cv::Mat::AUTO_STEP);
 
-        cv::Mat img = cv::Mat(h, w, CV_8UC4,
-            (void*)*sptr_i3d->getSensorImgData(), cv::Mat::AUTO_STEP);
-        utils::displayCV(img, sptr_i3d, start);
+        utils::cvDisplay(img, sptr_i3d, start);
     }
+
+    // ------> do stuff with tabletop environment <------
 
     k4aCaptureWorker.join();
     buildPCloudWorker.join();
-    findRegionWorker.join();
+    proposeRegionWorker.join();
     segmentRegionWorker.join();
-    frameRegionWorker.join();
     clusterRegionWorker.join();
-
     return 0;
 }
