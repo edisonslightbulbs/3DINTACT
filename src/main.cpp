9426495ebbbdf3f152b499a35acc5e32a34c0632
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-29 10:17:14 +0200

    cleaning main interface and improving learnability

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <chrono>
-#include <mutex>
 #include <string>
 #include <thread>
 #include <torch/script.h>
@@ -10,14 +9,7 @@
 #include "kinect.h"
 #include "macros.hpp"
 
-std::mutex m;
-
-void render(std::shared_ptr<i3d>& sptr_i3d)
-{
-    sptr_i3d->renderRegion(sptr_i3d);
-}
-
-void detect(std::shared_ptr<i3d>& sptr_intact)
+void findRegionObjects(std::shared_ptr<i3d>& sptr_intact)
 {
     std::vector<std::string> classNames;
     torch::jit::script::Module module;
@@ -25,23 +17,26 @@ void detect(std::shared_ptr<i3d>& sptr_intact)
     sptr_intact->findRegionObjects(classNames, module, sptr_intact);
 }
 
-void findRegion(std::shared_ptr<i3d>& sptr_i3d)
+void renderRegion(std::shared_ptr<i3d>& sptr_i3d)
 {
-    sptr_i3d->proposeRegion(sptr_i3d);
+    sptr_i3d->renderRegion(sptr_i3d);
 }
 
-void segment(std::shared_ptr<i3d>& sptr_i3d)
-{
-    sptr_i3d->segmentRegion(sptr_i3d);
-}
-
-void frame(std::shared_ptr<i3d>& sptr_i3d) { sptr_i3d->frameRegion(sptr_i3d); }
-
-void cluster(std::shared_ptr<i3d>& sptr_i3d)
+void clusterRegion(std::shared_ptr<i3d>& sptr_i3d)
 {
     int minPoints = 4;
     const float epsilon = 3.170;
     sptr_i3d->clusterRegion(epsilon, minPoints, sptr_i3d);
+}
+
+void segmentRegion(std::shared_ptr<i3d>& sptr_i3d)
+{
+    sptr_i3d->segmentRegion(sptr_i3d);
+}
+
+void proposeRegion(std::shared_ptr<i3d>& sptr_i3d)
+{
+    sptr_i3d->proposeRegion(sptr_i3d);
 }
 
 void buildPcl(std::shared_ptr<i3d>& sptr_i3d)
@@ -99,37 +94,34 @@ int main(int argc, char* argv[])
                  "for academic purposes!";
     LOG(INFO) << "-- press ESC to exit";
 
-    // initialize kinect
+    // initialize k4a kinect
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
 
-    // initialize the 3dintact API
+    // initialize the 3dintact
     std::shared_ptr<i3d> sptr_i3d(new i3d());
     sptr_i3d->raiseRunFlag();
 
-    // capture using depth sensor
+    // capture using k4a depth sensor
     std::thread k4aCaptureWorker(
         k4aCapture, std::ref(sptr_kinect), std::ref(sptr_i3d));
 
     // build point cloud
     std::thread buildPCloudWorker(buildPcl, std::ref(sptr_i3d));
 
-    // find region of interest
-    std::thread findRegionWorker(findRegion, std::ref(sptr_i3d));
+    // propose region
+    std::thread proposeRegionWorker(proposeRegion, std::ref(sptr_i3d));
 
-    // create GL and CV specific frames
-    std::thread frameRegionWorker(frame, std::ref(sptr_i3d));
+    // segment region
+    std::thread segmentRegionWorker(segmentRegion, std::ref(sptr_i3d));
 
-    // segment
-    std::thread segmentRegionWorker(segment, std::ref(sptr_i3d));
+    // render region
+    std::thread renderRegionWorker(renderRegion, std::ref(sptr_i3d));
 
-    // render
-    std::thread renderRegionWorker(render, std::ref(sptr_i3d));
+    // find objects in region
+    std::thread findRegionObjectsWorker(findRegionObjects, std::ref(sptr_i3d));
 
-    // find objects
-    std::thread findRegionObjectsWorker(detect, std::ref(sptr_i3d));
-
-    // cluster
-    std::thread clusterRegionWorker(cluster, std::ref(sptr_i3d));
+    // cluster segmented region
+    std::thread clusterRegionWorker(clusterRegion, std::ref(sptr_i3d));
 
     // ------> do stuff with tabletop environment <------
 
@@ -139,9 +131,8 @@ int main(int argc, char* argv[])
     buildPCloudWorker.join();
     renderRegionWorker.join();
     findRegionObjectsWorker.join();
-    findRegionWorker.join();
+    proposeRegionWorker.join();
     segmentRegionWorker.join();
-    frameRegionWorker.join();
     clusterRegionWorker.join();
     return 0;
 }
