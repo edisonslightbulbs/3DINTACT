bf35898724d8d44f9f82dd3a8b12b25324f78c29
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-06-03 12:32:45 +0200

    non-optimized example for using clusters

--- examples/cluster-use/main.cpp
+++ examples/cluster-use/main.cpp
@@ -1,13 +1,20 @@
+#include <Eigen/Dense>
 #include <chrono>
 #include <string>
 #include <thread>
 
-#include "helpers.h"
-#include "viewer.h"
 #include "i3d.h"
 #include "io.h"
 #include "kinect.h"
 #include "macros.hpp"
+#include "svd.h"
+#include "utilities.h"
+#include "viewer.h"
+
+void viewRegion(std::shared_ptr<I3d>& sptr_i3d)
+{
+    viewer::render(sptr_i3d); // CTRL + c to cycle different views
+}
 
 void clusterRegion(std::shared_ptr<I3d>& sptr_i3d)
 {
@@ -40,7 +47,7 @@ void k4aCapture(
     int w = k4a_image_get_width_pixels(sptr_kinect->m_depth);
     int h = k4a_image_get_height_pixels(sptr_kinect->m_depth);
 
-    while (sptr_i3d->isRun()) {
+    while (RUN) {
         START_TIMER
         sptr_kinect->capture();
         sptr_kinect->depthCapture();
@@ -104,18 +111,108 @@ int main(int argc, char* argv[])
     // cluster segmented region
     std::thread clusterRegionWorker(clusterRegion, std::ref(sptr_i3d));
 
+#define DEV_ENV 0
     // ------> do stuff with tabletop environment <------
+    SLEEP_UNTIL_CLUSTERS_READY
+    std::thread viewRegionWorker(viewRegion, std::ref(sptr_i3d));
 
-    SLEEP_UNTIL_SEGMENT_READY
-    viewer::render(sptr_i3d);
-    // use CTRL + c to cycle through different perspective views
+#if DEV_ENV == 0
+    while (RUN) {
 
+        // get clusters
+        auto clusters = sptr_i3d->getPCloudClusters();
+        auto points = clusters->first;
+        auto indexes = clusters->second;
+
+        // cast 'Index clusters' to 'Point clusters'
+        std::vector<std::vector<Point>> pointClusters;
+        for (const auto& cluster : indexes) {
+            std::vector<Point> heap;
+            for (const auto& index : cluster) {
+                heap.emplace_back(points[index]);
+            }
+            pointClusters.emplace_back(heap);
+        }
+
+        // config flags for svd computation
+        int flag = Eigen::ComputeThinU | Eigen::ComputeThinV;
+
+        // compute and heap the face normals of each cluster
+        std::vector<Eigen::Vector3d> normals;
+        for (const auto& cluster : pointClusters) {
+            SVD usv(cluster, flag);
+            normals.emplace_back(usv.getV3Normal());
+        }
+
+        // extract the vacant space and corresponding normal
+        std::vector<Point> vacantSpace = pointClusters[0];
+        Eigen::Vector3d n1 = normals[0];
+
+        pointClusters.erase(pointClusters.begin());
+        normals.erase(normals.begin());
+        const float ARGMIN = -0.00000008;
+
+        std::vector<std::vector<Point>> objectClusters;
+
+        // find coplanar clusters corresponding to the vacant surface space
+        int index = 0;
+        for (const auto& n2 : normals) {
+            double numerator = n1.dot(n2);
+            double denominator = n1.norm() * n2.norm();
+            double solution = std::acos(numerator / denominator);
+
+            if (!std::isnan(solution) && solution < ARGMIN
+                && solution > -ARGMIN) {
+                for (const auto& point : pointClusters[index]) {
+                    vacantSpace.emplace_back(point);
+                }
+            } else {
+                objectClusters.emplace_back(pointClusters[index]);
+            }
+            index++;
+        }
+
+        // max number of cluster regions
+        int max = 35;
+        index = 0;
+        std::vector<Point> pCloudSegment;
+        for (auto& object : objectClusters) {
+            if (index < max) {
+                for (auto& point : object) {
+                    pCloudSegment.emplace_back(point);
+                }
+                index++;
+            }
+        }
+
+        int w = sptr_i3d->getDepthWidth();
+        int h = sptr_i3d->getDepthHeight();
+
+        int16_t pCloud[w * h * 3];
+        uint8_t img_GL[w * h * 4];
+        uint8_t img_CV[w * h * 4];
+
+        uint8_t rgba[4] = { 27, 120, 55, 1 };
+
+        for (auto& point : vacantSpace) {
+            int id = point.m_id;
+            points[id].setPixel_GL(rgba);
+        }
+
+        for (int i = 0; i < points.size(); i++) {
+            utils::stitch(i, points[i], pCloud, img_GL, img_CV);
+        }
+
+        sptr_i3d->setColClusters({ pCloud, img_GL });
+    }
     // ------> do stuff with tabletop environment <------
+#endif
 
     k4aCaptureWorker.join();
     buildPCloudWorker.join();
     proposeRegionWorker.join();
     segmentRegionWorker.join();
     clusterRegionWorker.join();
+    viewRegionWorker.join();
     return 0;
 }
