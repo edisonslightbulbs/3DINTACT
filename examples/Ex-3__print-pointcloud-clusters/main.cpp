ff30b62a5caf3286f757373aa05f9ab9a9d75c2d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-06-23 13:53:54 +0200

    reviewing examples

--- examples/Ex-3__print-pointcloud-clusters/main.cpp
+++ examples/Ex-3__print-pointcloud-clusters/main.cpp
@@ -1,19 +1,17 @@
 #include <chrono>
 #include <string>
 #include <thread>
-#include <torch/script.h>
+#include <utilities.h>
 
 #include "i3d.h"
 #include "io.h"
 #include "kinect.h"
 #include "macros.hpp"
-#include "utilities.h"
-#include "yolov5.h"
 
 void clusterRegion(std::shared_ptr<I3d>& sptr_i3d)
 {
     int minPoints = 4;
-    const float epsilon = 3.170;
+    const float epsilon = 3.180;
     sptr_i3d->clusterRegion(epsilon, minPoints, sptr_i3d);
 }
 
@@ -37,9 +35,12 @@ void k4aCapture(
 {
     START
     sptr_kinect->capture();
+    sptr_kinect->imgCapture();
     sptr_kinect->depthCapture();
-    int w = k4a_image_get_width_pixels(sptr_kinect->m_depth);
-    int h = k4a_image_get_height_pixels(sptr_kinect->m_depth);
+    int imgWidth = k4a_image_get_width_pixels(sptr_kinect->m_img);
+    int imgHeight = k4a_image_get_height_pixels(sptr_kinect->m_img);
+    int depthWidth = k4a_image_get_width_pixels(sptr_kinect->m_depth);
+    int depthHeight = k4a_image_get_height_pixels(sptr_kinect->m_depth);
 
     while (sptr_i3d->isRun()) {
         START_TIMER
@@ -50,7 +51,8 @@ void k4aCapture(
         sptr_kinect->c2dCapture();
         sptr_kinect->transform(RGB_TO_DEPTH);
 
-        auto* ptr_k4aImgData = k4a_image_get_buffer(sptr_kinect->m_c2d);
+        auto* ptr_k4aC2dData = k4a_image_get_buffer(sptr_kinect->m_c2d);
+        auto* ptr_k4aImgData = k4a_image_get_buffer(sptr_kinect->m_img);
         auto* ptr_k4aPCloudData
             = (int16_t*)(void*)k4a_image_get_buffer(sptr_kinect->m_pcl);
         auto* ptr_k4aDepthData
@@ -59,9 +61,12 @@ void k4aCapture(
             = (k4a_float2_t*)(void*)k4a_image_get_buffer(sptr_kinect->m_xyT);
 
         // share k4a resources with intact
-        sptr_i3d->setDepthWidth(w);
-        sptr_i3d->setDepthHeight(h);
+        sptr_i3d->setImgWidth(imgWidth);
+        sptr_i3d->setImgHeight(imgHeight);
+        sptr_i3d->setDepthWidth(depthWidth);
+        sptr_i3d->setDepthHeight(depthHeight);
         sptr_i3d->setSensorImgData(ptr_k4aImgData);
+        sptr_i3d->setSensorC2DImgData(ptr_k4aC2dData);
         sptr_i3d->setSensorTableData(ptr_k4aTableData);
         sptr_i3d->setSensorDepthData(ptr_k4aDepthData);
         sptr_i3d->setSensorPCloudData(ptr_k4aPCloudData);
@@ -106,66 +111,45 @@ int main(int argc, char* argv[])
     std::thread clusterRegionWorker(clusterRegion, std::ref(sptr_i3d));
 
     // ------> do stuff with tabletop environment <------
+    SLEEP_UNTIL_POINTCLOUD_READY
+    std::vector<Point> pCloud = *sptr_i3d->getPCloud2x2Bin();
 
-    SLEEP_UNTIL_SENSOR_DATA_READY
-    std::vector<std::string> classnames;
-    torch::jit::script::Module module;
-    utils::configTorch(classnames, module);
+    SLEEP_UNTIL_PROPOSAL_READY
+    std::vector<Point> pCloudSeg = *sptr_i3d->getPCloudSeg2x2Bin();
 
-    uint8_t* ptr_img;
-    while (sptr_i3d->isRun()) {
-        clock_t start = clock();
-        int w = sptr_i3d->getDepthWidth();
-        int h = sptr_i3d->getDepthHeight();
+    SLEEP_UNTIL_CLUSTERS_READY
+    auto clusters = sptr_i3d->getPCloudClusters();
+    auto points = clusters->first;
+    auto indexClusters = clusters->second;
+    // points represents our segmented point cloud
+    // indexes variable represents clustered indexes of the
+    // points
 
-        ptr_img = *sptr_i3d->getSensorImgData();
+    // initialize cluster colors
+    std::vector<uint8_t*> colors;
+    utils::add(colors);
 
-        // create image frame
-        cv::Mat img;
-        cv::Mat frame
-            = cv::Mat(h, w, CV_8UC4, (void*)ptr_img, cv::Mat::AUTO_STEP)
-                  .clone();
-
-        // format frame for tensor input
-        cv::resize(frame, img, cv::Size(640, 384));
-        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
-        torch::Tensor imgTensor = torch::from_blob(
-            img.data, { img.rows, img.cols, 3 }, torch::kByte);
-        imgTensor = imgTensor.permute({ 2, 0, 1 });
-        imgTensor = imgTensor.toType(torch::kFloat);
-        imgTensor = imgTensor.div(255);
-        imgTensor = imgTensor.unsqueeze(0);
-
-        torch::Tensor preds // preds: [?, 15120, 9]
-            = module.forward({ imgTensor }).toTuple()->elements()[0].toTensor();
-        std::vector<torch::Tensor> dets
-            = yolo::non_max_suppression(preds, 0.4, 0.5);
-
-        // show objects
-        if (!dets.empty()) {
-            for (size_t i = 0; i < dets[0].sizes()[0]; ++i) {
-                float left
-                    = dets[0][i][0].item().toFloat() * (float)frame.cols / 640;
-                float top
-                    = dets[0][i][1].item().toFloat() * (float)frame.rows / 384;
-                float right
-                    = dets[0][i][2].item().toFloat() * (float)frame.cols / 640;
-                float bottom
-                    = dets[0][i][3].item().toFloat() * (float)frame.rows / 384;
-                float score = dets[0][i][4].item().toFloat();
-                int classID = dets[0][i][5].item().toInt();
-
-                cv::rectangle(frame,
-                    cv::Rect(left, top, (right - left), (bottom - top)),
-                    cv::Scalar(0, 255, 0), 2);
-                cv::putText(frame,
-                    classnames[classID] + ": " + cv::format("%.2f", score),
-                    cv::Point(left, top), cv::FONT_HERSHEY_SIMPLEX,
-                    (right - left) / 200, cv::Scalar(0, 255, 0), 2);
-            }
+    // colorize clusters: visualize clusters
+    for (auto& indexCluster: indexClusters){
+        int colIndex = utils::randNum((int)colors.size());
+        for(auto& index: indexCluster){
+            points[index].setPixel_RGBA(colors[colIndex]);
         }
-        utils::cvDisplay(frame, sptr_i3d, start);
     }
+    // helper script uses cloud compare for viewing
+    // output point clouds. To take advantage of the
+    // convenience script, install cloud compare.
+    // Alternatively use any other *.ply point cloud
+    // viewer. File written to: ./output/context.ply
+    ply::write(points);
+
+    // snapshot of tabletop environment
+    // File written to: ./output/scene.png
+    uint8_t* imgData = *sptr_i3d->getSensorImgData();
+    int w = sptr_i3d->getImgWidth();
+    int h = sptr_i3d->getImgHeight();
+    io::write(imgData, w, h);
+    STOP
     // ------> do stuff with tabletop environment <------
 
     k4aCaptureWorker.join();
