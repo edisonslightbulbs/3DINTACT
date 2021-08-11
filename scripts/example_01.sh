e70f1c7ec56553e82d7a5bbcba1249e6474a773a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-11 16:13:21 +0200

    :ant: :ant: debugging

--- scripts/example_01.sh
+++ scripts/example_01.sh
@@ -14,7 +14,8 @@ PROJECT_DIR=$(dirname $(dirname $(readlink -f "$0")))
 cd "$PROJECT_DIR" || return
 
 mkdir -p ./output
-rm -rf ./output/print-pointcloud
-./build/bin/print-pointcloud --logtostderr=1
+rm -rf ./output/show-pointcloud
+./build/bin/show-pointcloud --logtostderr=1
 
-cloudcompare.CloudCompare ./output/context.ply >/dev/null 2>&1 &
+cloudcompare.CloudCompare ./output/color.ply >/dev/null 2>&1 &
+cloudcompare.CloudCompare ./output/gray.ply >/dev/null 2>&1 &
