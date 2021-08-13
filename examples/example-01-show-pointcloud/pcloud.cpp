adc06b0faed963e9baa076f6e828eccb84b8a4ff
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-08-13 10:22:23 +0200

    ply.h -> pcl.h

--- examples/example-01-show-pointcloud/pcloud.cpp
+++ examples/example-01-show-pointcloud/pcloud.cpp
@@ -2,7 +2,7 @@
 
 #include "i3d.h"
 #include "kinect.h"
-#include "ply.h"
+#include "pcl.h"
 #include "scene.h"
 #include "usage.h"
 
@@ -13,12 +13,12 @@ int main(int argc, char* argv[])
     usage::prompt(ABOUT);
     std::shared_ptr<Kinect> sptr_kinect(new Kinect);
     std::shared_ptr<I3d> sptr_i3d(new I3d());
-    std::thread work(scene::init, std::ref(sptr_kinect), std::ref(sptr_i3d));
+    std::thread work(scene::context, std::ref(sptr_kinect), std::ref(sptr_i3d));
 
     // write non-color point cloud | fast 2x2 Bin
     WAIT_FOR_FAST_POINTCLOUD
     std::vector<Point> pCloud = *sptr_i3d->getPCloud2x2Bin();
-    ply::write(pCloud, "./output/gray.ply");
+    pcl::write(pCloud, "./output/gray.ply");
 
     // write color point cloud
     WAIT_FOR_COLOR_POINTCLOUD
@@ -26,7 +26,7 @@ int main(int argc, char* argv[])
     int h = sptr_i3d->getDHeight();
     int16_t* xyz = sptr_i3d->getXYZ()->data();
     uint8_t* rgba = sptr_i3d->getRGBA()->data();
-    ply::write(w, h, xyz, rgba, "./output/color.ply");
+    pcl::write(w, h, xyz, rgba, "./output/color.ply");
 
     sptr_i3d->stop();
     work.join();
