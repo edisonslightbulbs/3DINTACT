1dd80349e013c88b243d977f2f80721b429d6978
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-09-07 17:47:57 +0200

    :eyeglasses: code review

--- applications/app-01-procam-register-surface/reproject.cpp
+++ applications/app-01-procam-register-surface/reproject.cpp
@@ -4,8 +4,8 @@
 #include "i3dscene.h"
 #include "io.h"
 #include "kinect.h"
-#include "usage.h"
 #include "surface.h"
+#include "usage.h"
 
 int main(int argc, char* argv[])
 {
@@ -17,30 +17,40 @@ int main(int argc, char* argv[])
 
     // start i3d worker
     std::thread work(
-            i3dscene::context, std::ref(sptr_kinect), std::ref(sptr_i3d));
+        i3dscene::context, std::ref(sptr_kinect), std::ref(sptr_i3d));
 
     std::vector<cv::Mat> captures;
 
     // setup projector window
-    int w = 1366;
-    int h = 768;
+    int proWinWidth = 1366;
+    int proWinHeight = 768;
     const std::string window = "re-projection window";
-    surface::contrast(sptr_i3d, window, w, h, captures);
+    surface::contrast(sptr_i3d, window, proWinWidth, proWinHeight, captures);
 
-    // find area of projection (aop)
-    cv::Rect boundary
-            = surface::reg(captures[1], captures[0]);
-    cv::Mat background = captures[1];
+    // find region of interest / projection area on surface
+    cv::Rect roi;
+    roi = surface::reg(captures[1], captures[0]);
+    cv::Mat scene = captures[1];
 
-    // scene::undistort(background);
-    cv::Mat roi = background(boundary);
+    // todo scene::undistort(background);
+    cv::Mat projection = scene(roi);
+    // todo reproject aop
+    cv::Mat R, t; // todo
+    // todo scene::predistort(background);
 
-    // reproject aop
-    cv::Mat R, t;
-    // scene::predistort(background);
-    surface::project(window, w, h, roi, R, t);
-    // cv::imshow("Region of interest", roi);
+    while (RUN) {
+        int w = sptr_i3d->getDWidth();
+        int h = sptr_i3d->getDHeight();
+        uint8_t* c2dImage = *sptr_i3d->getC2DBGRAData();
+        cv::Mat frame = cv::Mat(h, w, CV_8UC4, (void*)c2dImage, cv::Mat::AUTO_STEP).clone();
 
+        //projection = projectionArea(sptr_i3d, roi);
+        projection = frame(roi);
+        surface::project(window, proWinWidth, proWinHeight, projection, R, t);
+        if (cv::waitKey(1) == 27) {
+            sptr_i3d->stop();
+        }
+    }
     work.join();
     return 0;
 }
